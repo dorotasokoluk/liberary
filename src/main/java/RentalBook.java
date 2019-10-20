755bb087d638cce3d61f1d039f280e596022a044
AniaZietek <annzet91@gmail.com>
2019-10-20 11:15:12 +0200

    Added RentalBook file

---
+++ src/main/java/RentalBook.java
@@ -0,0 +1,37 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+
+public class RentalBook {
+    private final List<Client> clientList = new ArrayList<>();
+    private final List<Book> bookList = new ArrayList<>();
+    private final List<Rental> rentalList = new ArrayList<>();
+
+    public void addClient(final Client client) {
+        Optional<Client> foundClient = clientList
+                .stream()
+                .filter(a -> a.getIdNumber().equals(client.getIdNumber()))
+                .findAny();
+        if (!foundClient.isPresent()) {
+            clientList.add(client);
+        }
+    }
+
+    public void addBook(Book book) {
+        Optional<Book> foundBook = bookList
+                .stream()
+                .filter(a -> a.getBookId().equals(book.getBookId()))
+                .findAny();
+        if (!foundBook.isPresent()) {
+            bookList.add(book);
+        }
+    }
+
+
+}
