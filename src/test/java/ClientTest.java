bc46890539cd46d526b5c3016df391288b17033d
Magda Czyz <stajniaenklawa@gmail.com>
2019-10-20 10:49:23 +0200

    2

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,5 +1,19 @@
+import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
-class ClientTest {
+public class ClientTest {
+    @Test
+    public void testClient() {
+        //given
+        Client client = new Client("Jan", "Kowalski", "ul. Szara 1", 1527L);
 
+        //when
+        //then
+        assertEquals("Jan", client.getFirstName());
+        assertEquals("Kowalski", client.getLastName());
+        assertEquals("ul. Szara 1", client.getAdress());
+        assertEquals(1527L, client.getIdNumber());
+    }
 }
