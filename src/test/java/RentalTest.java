124d2abb23229fccd60a0ddf06261ffdfe87da24
dorotasokoluk@gmail.com <Lachudra01/>
2019-10-20 10:43:03 +0200

    Add Client Class

---
+++ src/test/java/RentalTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class RentalTest {
+    @Test
+    public void testRental(){
+        //given
+        String pattern="dd-mm-yyyy";
+
+
+
+        Client client = new Client("Adam", "Małysz", "Koncertowa 20", 15L);
+        //Book book = new Book("Władca Pierścieni", "Tolkien", "2", )
+
+        //when
+
+        //then
+    }
+
+}
