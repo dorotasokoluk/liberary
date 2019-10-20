3dceeda0fc0c67596c827693a3a9fc8f09e11b2f
dorotasokoluk@gmail.com <Lachudra01/>
2019-10-20 11:05:24 +0200

    Add Client Class

--- src/test/java/RentalTest.java
+++ src/test/java/RentalTest.java
@@ -1,19 +1,32 @@
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
 public class RentalTest {
     @Test
-    public void testRental(){
+    public void testRental() throws ParseException {
         //given
-        String pattern="dd-mm-yyyy";
-
-
+        String pattern = "dd-mm-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date dateFrom = simpleDateFormat.parse("2019-10-20");
+        Date dateTo = simpleDateFormat.parse("2019-10-31");
 
         Client client = new Client("Adam", "Małysz", "Koncertowa 20", 15L);
-        //Book book = new Book("Władca Pierścieni", "Tolkien", "2", )
+        Book book = new Book("Władca Pierścieni", "Tolkien", "2", "1946");
+        Rental rental = new Rental(client, book, dateFrom, dateTo);
 
         //when
 
         //then
+        assertEquals(client, rental.getClient());
+        assertEquals(book, rental.getBook());
+        assertEquals(dateFrom, rental.getDateFrom());
+        assertEquals(dateTo, rental.getDateTo());
     }
 
 }
