a7f1a2b099ecd32505dc150913476adcc2747da4
dorotasokoluk <dorotasokoluk@gmail.com>
2019-10-26 11:08:05 +0200

    gitignore

--- src/main/java/Body/Rental.java
+++ src/main/java/Body/Rental.java
@@ -3,6 +3,7 @@ package Body;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Calendar;
 import java.util.Date;
 
 @Getter
@@ -13,4 +14,15 @@ public class Rental {
     private Book book;
     private Date dateFrom;
     private Date dateTo;
+
+
+    public long  daysBetween (Date dateFrom, Date dateTo) {
+        Calendar date = (Calendar) dateFrom.clone();
+        long daysBetween = 0;
+        while (date.before(dateTo)) {
+            date.add(Calendar.DAY_OF_MONTH, 1);
+            daysBetween++;
+        }
+        return daysBetween;
+    }
 }
--- src/test/java/Body/RentalTest.java
+++ src/test/java/Body/RentalTest.java
@@ -13,7 +13,7 @@ public class RentalTest {
     @Test
     public void testRental() throws ParseException {
         //given
-        String pattern = "dd-mm-yyyy";
+        String pattern = "yyyy-mm-dd";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         Date dateFrom = simpleDateFormat.parse("2019-10-20");
         Date dateTo = simpleDateFormat.parse("2019-10-31");
@@ -29,6 +29,15 @@ public class RentalTest {
         assertEquals(book, rental.getBook());
         assertEquals(dateFrom, rental.getDateFrom());
         assertEquals(dateTo, rental.getDateTo());
+    }
+
+    @Test
+    public void daysBetween (){
+        //given
+
+        //when
+
+        //then
     }
 
 }
